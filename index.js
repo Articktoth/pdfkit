379bc3c681234fe5e25e69ecd7104aaef6986f7c
Alejandro Soto <tothdevelop@gmail.com>
2020-07-18 20:30:08 -0400

    probando pdfkit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
 
 const testjson = require('./test.json');
 
 const doc = new PDFDocument({
-    size: [200, 350],
+    size: [350, 350],
     margins: {
         top: 20,
         bottom: 20,
@@ -17,23 +18,28 @@ const datosEmisor = testjson.Documento.Encabezado.Emisor;
 const idDoc = testjson.Documento.Encabezado.IdDoc;
 const items = testjson.Documento.Detalle;
 
-doc.pipe(fs.createWriteStream('./tests/test2.pdf'));
+doc.pipe(fs.createWriteStream('./tests/test.pdf'));
 
 doc
     .fontSize(10)
-    .text(datosEmisor.RznSoc, {align: "center"})
-    .moveDown()
+    .text(datosEmisor.RznSoc, { align: "center" })
     .text(`${datosEmisor.DirOrigen} ${datosEmisor.CmnaOrigen} ${datosEmisor.CiudadOrigen}`)
     .text(datosEmisor.GiroEmis)
-    .text(`FECHA EMISION : ${idDoc.FchEmis}`);
-
-const table = {
-    rows: []
-};
+    .text(`FECHA EMISION : ${idDoc.FchEmis}`)
+    .moveDown();
 
 for (item of items) {
-    doc.text(item.NmbItem)
+    doc.text(`${item.NmbItem} ${item.PrcItem}`);
 }
+
+
+
+doc
+    .text(`DEBITO ONLINE:  ${testjson.Adicionales.debitoOnline}`)
+    .text(`TOTAL:  ${testjson.Adicionales.debitoOnline}`)
+    .moveDown()
+    .text(`ATENDIDO POR: ${testjson.Adicionales.atendidoPor}`);
+
 
 
 doc.end();
