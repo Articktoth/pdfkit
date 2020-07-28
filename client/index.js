0c4516e9f692e95f8a1f445ade1262e0544e0744
Alejandro Soto <tothdevelop@gmail.com>
2020-07-28 02:32:55 -0400

    pdf con detalles y sin detalles

---
+++ client/index.js
@@ -0,0 +1,84 @@
+let razonSocial = document.getElementById('inputRznSocial').value;
+let rut = document.getElementById('rut').value;
+let giroEmisor = document.getElementById('giroEmisor').value;
+let selector = document.getElementById("classSelect");
+selector.addEventListener("change", addClasses);
+
+let myCheck = document.getElementById('myCheck');
+let details = false;
+function añadirDetalles(){
+    if(myCheck.checked == true){
+        details = true;
+    }else if (myCheck.checked == false) {
+        details= false;
+    }
+    console.log(details)
+}
+
+
+const url = 'http://localhost:5000';
+
+let oldClass = "";
+
+function addClasses() {
+    let content = document.getElementById('title');
+    let newClass = "move-to-" + this.value;
+    if (oldClass) {
+        content.classList.remove(oldClass);
+    }
+
+    content.classList.add(newClass);
+
+    oldClass = newClass;
+}
+
+
+const downloadPdf = async () => {
+    event.preventDefault();
+
+    let razonSocial = document.getElementById('inputRznSocial').value;
+    let rut = document.getElementById('inputRut').value;
+    let giroEmisor = document.getElementById('giroEmisor').value;
+
+    let args = document.getElementById('classSelect');
+    let valueargs = args.options[args.selectedIndex].value;
+
+    console.log(valueargs)
+
+    const data = {
+        razonSocial: razonSocial,
+        rut: rut,
+        giroEmisor: giroEmisor,
+        details: details,
+        options: {
+            option1: valueargs
+        }
+    }
+
+    fetch(`${url}/create-pdf`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(res => {
+            res.json()
+        })
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response));
+}
+
+
+function getVal() {
+    let razonSocial = document.getElementById('inputRznSocial').value;
+    let rut = document.getElementById('inputRut').value;
+    let giroEmisor = document.getElementById('giroEmisor').value;
+
+    document.getElementById('title').innerHTML = razonSocial;
+    document.getElementById('rut').innerHTML = rut;
+    document.getElementById('ejemplo').innerHTML = giroEmisor;
+}
+
+
+
--- controller/pdfcontroller.js
+++ controller/pdfcontroller.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
 
-const PDFDocument = require('../pdfkit-tables');
+const PDFDocument = require('pdfkit');
 const fs = require('fs');
 const testjson = require('../test.json');
 
 
 app.post('/create-pdf', (req, res) => {
     const razonSocial = req.body.razonSocial;
-    const dirOrigen = req.body.dirOrigen;
+    const rut = req.body.rut;
     const giroEmisor = req.body.giroEmisor;
+    const options = req.body.options;
+    const details = req.body.details;
+    console.log(details)
 
     const doc = new PDFDocument(
         {
-            size: [226, 300],
+            size: [226, 400],
             margins: {
                 top: 20,
                 bottom: 20,
@@ -26,16 +29,18 @@ app.post('/create-pdf', (req, res) => {
     const idDoc = testjson.Documento.Encabezado.IdDoc;
     const items = testjson.Documento.Detalle;
 
-    doc.pipe(fs.createWriteStream('./tests/testpostman.pdf'));
+    doc.pipe(fs.createWriteStream(`./tests/ifdetailsfalse.pdf`));
 
     doc
         .fontSize(10)
-        .text(`${razonSocial}`, { align: "center" })
-        .text(`${dirOrigen}`, { align: "left" })
-        .text(`${giroEmisor}`, { align: "left" })
-        .text(`FECHA EMITIDA : ${idDoc.FchEmis}`, { align: "left" })
+        .text(`${razonSocial}`, { align: `${options.option1}` })
+        .text(`${rut}`, { align: `${options.option2}` })
+        .text(`${giroEmisor}`, { align: `${options.option3}` })
+        .text(`FECHA EMISION: ${idDoc.FchEmis}`, { align: "left" })
         .moveDown();
 
+    //DEtalles
+        if (details == true) {
         for (i = 0; i < items.length; i++) {
             const item = items[i];
             const position = 100 + (i + 1) * 10;
@@ -43,35 +48,26 @@ app.post('/create-pdf', (req, res) => {
                 .text(item.NmbItem, 10, position)
                 .text(item.PrcItem, 10, position, { align: 'right' });
         }
-        doc.moveDown();
-        
-        doc
-            .text('DEBITO', 10, 200)
-            .text('precio', 10, 200, { align: 'right' })
-            .text('TOTAL', 10, 210,)
-            .text('PRECIO', 10, 210, { align: 'right' });
+    }
 
-    // doc
-    //     .text(`DEBITO ONLINE:  ${testjson.Adicionales.debitoOnline}`, { align: "left" })
+    doc.moveDown();
 
-    //     .text(`${item.NmbItem}:  ${item.PrcItem}`, { width: 100, align: 'left' })
-    //     .text(`${item.PrcItem}`, 50, 50, { width: 100, align: 'right' })
+    doc
+        .text('DEBITO', 10, 200)
+        .text('precio', 10, 200, { align: 'right' })
+        .text('TOTAL', 10, 210,)
+        .text('PRECIO', 10, 210, { align: 'right' });
 
-    //     .text(`TOTAL:  ${testjson.Adicionales.debitoOnline}`, { align: "left" })
-    //     .moveDown()
-    //     .text(`ATENDIDO POR: ${testjson.Adicionales.atendidoPor}`, { align: "left" })
-    //     .moveDown();
-
-    doc.image('images/test2.png', {
-        fit: [200, 300],
-        align: 'center'
+    doc.image('images/test.png', {
+        fit: [200, 150],
+        align: 'left'
     });
 
     doc.end();
 
     res.json({
         razonSocial,
-        dirOrigen,
+        rut,
         giroEmisor
     });
 
