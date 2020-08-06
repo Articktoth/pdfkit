0fc31d3708b34bc4eeaccd191a69cba38e0d51b3
Alejandro Soto <tothdevelop@gmail.com>
2020-08-06 15:50:54 -0400

    cambio de estilos y funcion format agregada

--- controller/createpdf.js
+++ controller/createpdf.js
@@ -48,7 +48,7 @@ function generateHeader(doc, datajson, plantilla) {
     doc
 
         .font(docHead.Rut.font)
-        .text(datajson.Documento.Encabezado.Emisor.RUTEmisor, { align: docHead.DirOrigen })
+        .text(datajson.Documento.Encabezado.Emisor.RUTEmisor, { align: docHead.Rut.align })
         .moveDown();
 
     doc
@@ -56,8 +56,11 @@ function generateHeader(doc, datajson, plantilla) {
         .font(docHead.Folio.font)
         .text(`FOLIO: ${datajson.Documento.Encabezado.IdDoc.Folio}`);
 
+    const array = datajson.Documento.Encabezado.IdDoc.FchEmis.split('-');
+    const fechaFormato = `${array[2]}/${array[1]}/${array[0]}`;
+
     doc.font(docHead.Fecha.font)
-        .text(`FECHA EMISION: ${datajson.Documento.Encabezado.IdDoc.FchEmis}`, { align: docHead.Fecha.align })
+        .text(`FECHA EMISION: ${fechaFormato}`, { align: docHead.Fecha.align })
         .moveDown();
 }
 
@@ -73,38 +76,46 @@ function generateDetails(doc, datajson, plantilla) {
         .text("DESCRIPCION", 10, position, { align: "center" })
         .text("VALOR", 10, position, { align: "right" })
 
+    doc
+        .font(docDetails.Items.font)
+        .text(`----------------------------------`)
+
     for (i = 0; i < items.length; i++) {
         const item = items[i];
         const position = 100 + (i + 1) * 15;
+
         doc
-            .text(item.QtyItem, 20, position)
-            .text(item.NmbItem, 10, position, { align: 'center' })
-            .text(item.PrcItem, 10, position, { align: docDetails.Precio.align });
+            .fontSize(8)
+            .text(item.QtyItem, 20, position, { lineBreak: false })
+            .text(item.NmbItem.slice(0, 25), 10, position, { align: 'center' })
+            .text(format(item.MontoItem), 10, position, { align: docDetails.Precio.align });
     }
     doc.moveDown();
-    generateTotales(doc, datajson, plantilla);
+    doc
+        .fontSize(10)
+        .font(docDetails.Items.font)
+        .text(`----------------------------------`)
+
+    generateTotales(doc, datajson, plantilla, undefined);
 }
 
 function generateTotales(doc, datajson, plantilla, position) {
     let docTotals = plantilla.Plantilla.Totales;
 
     doc
-        .text("TOTAL",10, position, {align: "left", lineBreak: false})
-        .text(datajson.Documento.Encabezado.Totales.MntTotal, {align: "right"})
+        .text("TOTAL", 10, position, { align: "left", lineBreak: false })
+        .text(`$${format(datajson.Documento.Encabezado.Totales.MntTotal)}`, { align: "right" })
         .moveDown();
 }
-
-
 
 function generateFooter(doc, plantilla, docConfig, height, timbrepng, datajson) {
 
     let docFooter = plantilla.Plantilla.FooterTimbre;
 
     doc
-        .font("Courier-Bold")
-        .text(`El IVA `, { align: "center", lineBreak: false })
         .font("Courier")
-        .text(`de esta boleta es ${datajson.Documento.Encabezado.Totales.IVA}`)
+        .fontSize(8)
+        .text(`El IVA de esta boleta es $${format(datajson.Documento.Encabezado.Totales.IVA)}`, { align: "left", lineBreak: false })
         .moveDown();
 
     doc.image(timbrepng, 10, height - 120, {
@@ -114,6 +125,20 @@ function generateFooter(doc, plantilla, docConfig, height, timbrepng, datajson) 
         align: docFooter.align
     })
 }
+
+function format(monto){
+
+    var num = monto.toString().replace(/\./g,'');
+    if(!isNaN(monto)){
+        num = num.toString().split('').reverse().join('').replace(/(?=\d*\.?)(\d{3})/g,'$1.');
+        num = num.split('').reverse().join('').replace(/^[\.]/,'');
+    }else{ 
+        num =  monto.toString().replace(/[^\d\.]*/g,'')
+    }
+
+    return num
+}
+
 
 
 module.exports = {
